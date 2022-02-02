74a0a8c661c5a7f9e0ad3158a2c1412d158b73e6
Divyansh Kumar <divyansh@hubilo.com>
2022-02-02 18:25:53 +0530

    build as per duda

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,17 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+let loadContainer: Element | DocumentFragment
+export function init({ container, props = {} }: any = {}) {
+  loadContainer = container
+  ReactDOM.render(<App {...props} />, container)
+}
+export function clean() {
+  if (loadContainer) {
+    ReactDOM.unmountComponentAtNode(loadContainer)
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  init({ container: document.getElementById('root') })
+}
